dc97b1c68e2e5aa180ce4aaa771a300a1ee96fef
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-21 18:59:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,16 +1,4 @@
 def using_push(array,string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    colors_in_the_rainbow.push("violet")
+  array.push(string)
 end
 
-def using_unshift(array,string)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.unshift("Staten Island")
-end
-
-def using_pop(array)
-  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  deletedelement=continents.pop()
-end
-  
-  
